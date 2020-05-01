9f9b82db434af0fc5934d3ebcbe7754b02794a84
Swaraj <swarajksantra@gmail.com>
2020-05-01 15:46:07 +0530

    correction

--- LibMgmt/LibMgmt.UI/AccountView.xaml.cs
+++ LibMgmt/LibMgmt.UI/AccountView.xaml.cs
@@ -16,19 +16,13 @@ using System.Windows.Shapes;
 namespace LibMgmt.UI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for AccountView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AccountView : UserControl
     {
-        public MainWindow()
+        public AccountView()
         {
             InitializeComponent();
-            //
-
-            if (MainContainer.HasContent)
-                MainContainer.Content = null;
-
-            MainContainer.Content = new LoginView();
         }
     }
 }
--- LibMgmt/LibMgmt.UI/HomeView.xaml.cs
+++ LibMgmt/LibMgmt.UI/HomeView.xaml.cs
@@ -16,19 +16,13 @@ using System.Windows.Shapes;
 namespace LibMgmt.UI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomeView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomeView : UserControl
     {
-        public MainWindow()
+        public HomeView()
         {
             InitializeComponent();
-            //
-
-            if (MainContainer.HasContent)
-                MainContainer.Content = null;
-
-            MainContainer.Content = new LoginView();
         }
     }
 }
