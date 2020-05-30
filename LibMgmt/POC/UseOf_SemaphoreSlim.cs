fbf0e2f4703c221709d1f38a21286fb202dedff7
Swaraj <swarajksantra@gmail.com>
2020-05-30 23:27:50 +0530

    use of semaphoreslim

---
+++ LibMgmt/POC/UseOf_SemaphoreSlim.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace POC
+{
+    //[Run]
+    public class UseOf_SemaphoreSlim : Runnable
+    {
+        private void SemaphoreLock(SemaphoreSlim semaphoreSlim, Action action, CancellationToken cancellationToken)
+        {
+            semaphoreSlim.Wait(cancellationToken);
+            try
+            {
+                action();
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+        public override void Run(string[] args)
+        {
+            var ss = new SemaphoreSlim(1);
+            SemaphoreLock(ss, () => { /* do something */ }, CancellationToken.None);
+        }
+    }
+}
